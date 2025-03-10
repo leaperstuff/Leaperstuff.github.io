03d12a146c0d46ea713cee9696675799a4f84d04
Ivaan Srivastava <ivaansri.contact@gmail.com>
2025-03-10 21:25:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,25 +5,3 @@ function toggleMenu() {
 
 
 
-// Initialize Tawk.to with delayed chat creation
-var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-
-(function() {
-    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-    s1.async = true;
-    s1.src = 'https://embed.tawk.to/67cef0441516c119182fb70a/1im05l3j3';
-    s1.charset = 'UTF-8';
-    s1.setAttribute('crossorigin', '*');
-    s0.parentNode.insertBefore(s1, s0);
-})();
-
-// Ensure the chat widget is only loaded when the user clicks "Chat with Us"
-Tawk_API.onLoad = function() {
-    Tawk_API.hideWidget(); // Hide widget initially
-};
-
-// Function to show chat widget when button is clicked
-function showChat() {
-    Tawk_API.showWidget();
-    Tawk_API.maximize();
-}
