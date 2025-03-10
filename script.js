8a674f37f00aebbe08873687cf0b2a83f2dc74df
Ivaan Srivastava <ivaansri.contact@gmail.com>
2025-03-10 21:28:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 function toggleMenu() {
-            var menu = document.querySelector(".menu");
-            menu.style.display = (menu.style.display === "flex") ? "none" : "flex";
-        }
-
-
-
+    document.querySelector(".menu").classList.toggle("active");
+}
